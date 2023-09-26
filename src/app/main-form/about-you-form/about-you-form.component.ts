cdd8bacd5ec20c0976d71605addf17063e90666b
Cristian Calderón Sanchez <cristian.calderons@sansano.usm.cl>
2023-09-26 06:50:46 -0300

    feat(final-structure): add localstorage to aboutyouform

--- src/app/main-form/about-you-form/about-you-form.component.ts
+++ src/app/main-form/about-you-form/about-you-form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-about-you-form',
   templateUrl: './about-you-form.component.html',
   styleUrls: ['./about-you-form.component.css']
 })
-export class AboutYouFormComponent {
+export class AboutYouFormComponent implements OnInit {
+  public aboutYouForm = {
+    name: '',
+    lastName: '',
+    email: '',
+    phone: '',
+  };
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+  saveAboutYou() {
+    localStorage.setItem('aboutYouForm)', JSON.stringify(this.aboutYouForm));
+  }
 
 }
--- src/app/main-form/ods-input/ods-input.component.ts
+++ src/app/main-form/ods-input/ods-input.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./ods-input.component.css']
 })
 export class OdsInputComponent {
+  images: any[] = [
+    { src: '../../../assets/images/ods/S_SDG_PRINT-01.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-02.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-03.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-04.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-05.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-06.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-07.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-08.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-09.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-10.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-11.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-12.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-13.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-14.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-15.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-16.jpg', isSelected: false },
+    { src: '../../../assets/images/ods/S_SDG_PRINT-17.jpg', isSelected: false },
+  ];
 
+  toggleSelection(image: any) {
+    image.isSelected = !image.isSelected;
+  }
 }
